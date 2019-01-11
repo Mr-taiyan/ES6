7f08ece0673ff3345790b2a44f8c2f317d0076fe
chen <cty9388@hotmail.com>
2019-01-11 14:33:25 +0800

    to check if object's attributes name can be computed

---
+++ obj/computedName.js
@@ -0,0 +1,9 @@
+var person = {},
+    lastName = "last name";
+
+person["first name"] = "chen";
+person[lastName] = "taiyan";
+
+console.log(person["first name"]);
+// console.log(person[lastName]);
+console.log(person["last name"]);
