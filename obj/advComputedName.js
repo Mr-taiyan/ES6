360782d56b91de660b9ab71802f062af8dfb495d
chen <cty9388@hotmail.com>
2019-01-12 13:41:37 +0800

    update advComputedeNames to verify the computed process

--- obj/advComputedName.js
+++ obj/advComputedName.js
@@ -1,13 +1,19 @@
 var person = {},
     lastName = "last name",
-    fullName = "fullName";
+    fullName = "fullName",
+    ceshi = 'hehe';
 
 person["first name"] = "chen";
 person[lastName] = "taiyan";
 person[fullName] = "chentaiyan";
+person[ceshi] = '只是一个测试';
 
 console.log(person["first name"]); // chen
 console.log(person[lastName]); // taiyan
 console.log(person.lastName); //undefined
 console.log(person[fullName]); // chentaiyan
 console.log(person.fullName); // chentaiyan
+
+console.log(person[ceshi]);
+console.log(person.ceshi); // undefined
+console.log(person.hehe);
