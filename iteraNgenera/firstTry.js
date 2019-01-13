a6003979479de86abe8a5e3c7a408c0c91c2e410
chen <cty9388@hotmail.com>
2019-01-13 15:35:56 +0800

    add firstTry.js to see the essence of iterable object

---
+++ iteraNgenera/firstTry.js
@@ -0,0 +1,29 @@
+let collection = {
+    items: [],
+    *[Symbol.iterator]() {
+        for (let item of this.items) {
+            yield item;
+        }
+    }
+};
+
+collection.items.push(1);
+collection.items.push(2);
+collection.items.push(3);
+
+for (let x of collection) {
+    console.log(x);
+}
+
+collection.items = [4,5,6];
+
+for (let x of collection) {
+    console.log(x);
+}
+
+let iterator = collection[Symbol.iterator]();
+// console.log(iterator);
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
