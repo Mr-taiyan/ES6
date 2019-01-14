0da67a223bdf4a2edce011640eeae54507a25301
chen <cty9388@hotmail.com>
2019-01-14 22:55:06 +0800

    update args.js to see what happens when next() is more than yield operator

--- iteraNgenera/args.js
+++ iteraNgenera/args.js
@@ -1,13 +1,14 @@
 function* createIterator() {
     let first = yield 1;
-    console.log('chen');
+    console.log(first);
     let second = yield first + 2;
-    yield second + 3;
+    let ceshi = yield second + 3;
+    console.log(ceshi);
 }
 
 let iterator = createIterator();
 
 console.log(iterator.next());
-// console.log(iterator.next(4));
-// console.log(iterator.next(5));
-// console.log(iterator.next());
+console.log(iterator.next(4));
+console.log(iterator.next(5));
+console.log(iterator.next(1));
