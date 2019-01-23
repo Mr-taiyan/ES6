556ef4401d8b2d7bf880751230d47cb54eb2f82a
chen <cty9388@hotmail.com>
2019-01-24 00:48:50 +0800

    complete promise primitively

---
+++ promise/implementOfPromise/src/Es6Promise.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const STATE = {
+    pending: 0,
+    resolved: 1,
+    rejected: 2
+};
+
+class Es6Promise {
+    constructor(executor) {
+        this._state = STATE.pending;
+        this._value = undefined;
+        this._callback = [];
+
+        if (typeof executor === "function") {
+            let resolve = (value) => {
+                this._transition(STATE.resolved, value);
+            };
+
+            let reject = (value) => {
+                this._transition(STATE.rejected, value);
+            };
+
+            executor(resolve, reject);
+        }
+    }
+
+    _transition(state, value) {
+        if (this._state === STATE.pending) {
+            this._state = state;
+            this._value = value;
+            this._callback.forEach(callback => callback());
+        }
+
+    }
+}
