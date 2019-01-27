bb65ba7412bbfdb5cf828604ff276973931a045e
chen <cty9388@hotmail.com>
2019-01-27 20:55:32 +0800

    add an example about bind.js

---
+++ ecapusulaing/bind.js
@@ -0,0 +1,9 @@
+import {name, setName, setValue} from "./modExamples/exampleForBind";
+
+console.log(name);
+setName("chentaiyan");
+console.log(name);
+
+let value = 8;
+setValue(80);
+console.log(value);
---
+++ ecapusulaing/modExamples/exampleForBind.js
@@ -0,0 +1,12 @@
+export var name = "chen";
+// export function setName(name) {
+//     name = name;
+// }
+export function setName(newName) {
+    name = newName;
+}
+
+let value = 10;
+export function setValue(newValue) {
+    value = newValue;
+}
