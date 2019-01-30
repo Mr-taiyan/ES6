a1dee8327e18ca488a8315726c2cb9c33ed1aa9e
chen <cty9388@hotmail.com>
2019-01-30 14:56:42 +0800

    experimental modules

---
+++ ecapusulaing/ceshi.mjs
@@ -0,0 +1,5 @@
+import {color, Ceshi} from "./first.mjs";
+
+console.log(color);
+
+new Ceshi('chen').sayName();
---
+++ ecapusulaing/first.mjs
@@ -0,0 +1,11 @@
+export var color = 'red';
+
+export class Ceshi {
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayName() {
+        console.log(this.name);
+    }
+}
