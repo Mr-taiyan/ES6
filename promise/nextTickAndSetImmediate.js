4f1e3cd35de81fdd7da5592aa3bddec1458ee9a9
chen <cty9388@hotmail.com>
2019-03-01 10:27:46 +0800

    new file to see distiction between nextTick nad setImmediate

---
+++ promise/nextTickAndSetImmediate.js
@@ -0,0 +1,13 @@
+process.nextTick(() => {
+    console.log('nextTick');
+});
+
+Promise.resolve().then(() => {
+    console.log('then');
+});
+
+setImmediate(() => {
+    console.log('setImmediate');
+});
+
+console.log('end');
